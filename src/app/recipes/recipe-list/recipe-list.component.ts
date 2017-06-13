84d9c2a337d34c543b921570a284f1333e48154a
Jaime Quintero <jaimeq22@Jaimes-MacBook-Pro.local>
2017-06-13 02:01:36 -0400

    latest changes on recipe list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,7 +7,10 @@ import { Recipe } from '../recipe.model'
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  Recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+  new Recipe('T-Recipe','a recipe for testing',
+'http://www.seriouseats.com/recipes/assets_c/2016/05/20160503-fava-carrot-ricotta-salad-recipe-1-thumb-1500xauto-431710.jpg')
+];
 
   constructor() { }
 
